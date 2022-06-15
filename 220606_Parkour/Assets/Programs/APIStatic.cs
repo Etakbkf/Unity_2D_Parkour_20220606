19ddcc963084af63ef33406b94ce91324c8baeb4
Etakbkf <103544200+Etakbkf@users.noreply.github.com>
2022-06-15 08:18:24 +0800

    update homework

--- 220606_Parkour/Assets/Programs/APIStatic.cs
+++ 220606_Parkour/Assets/Programs/APIStatic.cs
@@ -24,7 +24,7 @@ namespace Ash
             //類別.靜態屬性名稱 指定 值;
             Cursor.visible = false;
             Physics2D.gravity = new Vector2(0, -1.0f);     //x值是左右,y值是上下,負值是向下正值是向上
-            Time.timeScale = 0.5f;                        //數字小是慢動作,數字大是加速
+           // Time.timeScale = 0.5f;                        //數字小是慢動作,數字大是加速
 
             //使用靜態方法
             //語法:
@@ -36,7 +36,18 @@ namespace Ash
             int camCount = Camera.allCamerasCount;
             print("攝影機數量:" + camCount);
             print("應用程式平台:" + Application.platform);
+            Physics.sleepThreshold = 10.0f;
+            print("睡眠臨界值:" + Physics.sleepThreshold);
+            Time.timeScale = 0.5f;
+            print("9.999四捨五入:" + Mathf.Round(9.999f));
+            Vector3 avector;
+            Vector3 bvector;
+            avector = new Vector3(1, 1, 1);
+            bvector = new Vector3(22, 22, 22);
+            print("(1,1,1)與(22,22,22)兩點距離為:" + Vector3.Distance(avector,bvector));
+            Application.OpenURL("http://unity3d.com/");
             #endregion
+
         }
 
 
@@ -45,13 +56,16 @@ namespace Ash
         private void Update()
         {
             #region  課堂
+            /*
             int rngInt = Random.Range(1, 3);
             print("隨機範圍:" + rngInt);
+            */
             #endregion
             #region  習作
             if (Input.anyKey) { print("按鍵:"+Input.anyKey); }
-
-
+            float timed = Time.realtimeSinceStartup;
+            print("<color=#0066CC>經過時間:</color>" + timed );
+            if (Input.GetKey("space"))  { print("<color=yellow>已按下空白鍵</color>");  }
             #endregion
         }
 
